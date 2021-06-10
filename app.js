f24d2f14cfe4be7508626a298ce6b65d7ed1b891
Bruno Oliveira <bruno.deao@gmail.com>
2021-06-10 17:45:09 -0300

    endpoints docs based on openapi

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import 'dotenv/config';
 import express from 'express';
 import bodyParser from 'body-parser';
+import {app as router} from './src/routes/index.js';
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use(router);
 
 app.listen(port, err => {
   if (err) {
